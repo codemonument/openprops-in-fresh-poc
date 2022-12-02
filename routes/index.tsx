72401a19cd114cabf511dfb25dacf365c82dfdba
Benjamin Jesuiter <bjesuiter@gmail.com>
2022-12-02 17:01:20 +0100

    add global.css

--- routes/index.tsx
+++ routes/index.tsx
@@ -6,6 +6,7 @@ export default function Home() {
     <>
       <Head>
         <title>Fresh App</title>
+        <link rel="stylesheet" href="/global.css"></link>
       </Head>
       <div>
         <img
