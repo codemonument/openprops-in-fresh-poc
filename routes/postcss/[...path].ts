a53a362a803a06718e3faf272dcb8783269dd3fe
Benjamin Jesuiter <bjesuiter@gmail.com>
2022-12-03 01:07:11 +0100

    add standalone file for transforming css files with postcss & openprops

--- routes/postcss/[...path].ts
+++ routes/postcss/[...path].ts
@@ -1,11 +1,11 @@
-import { HandlerContext } from "$fresh/server.ts";
-import postcss from "https://deno.land/x/postcss@8.4.16/mod.js";
-
 /**
  * Configures postcss with openprops as standalone file
  * (copy-pasteable)
  */
 
+import { HandlerContext } from "$fresh/server.ts";
+import * as log from "https://deno.land/std@0.167.0/log/mod.ts";
+import postcss from "https://deno.land/x/postcss@8.4.16/mod.js";
 // See: https://www.npmjs.com/package/postcss-jit-props
 import postcssJitProps from "npm:postcss-jit-props@1.0.8";
 // See: https://www.npmjs.com/package/open-props
@@ -13,13 +13,27 @@ import OpenProps from "npm:open-props@1.5.1";
 
 export const postCssInstance = postcss([postcssJitProps(OpenProps)]);
 
+log.setup({
+  handlers: {
+    console: new log.handlers.ConsoleHandler("DEBUG"),
+  },
+  loggers: {
+    "postcss_resource_route": {
+      level: "DEBUG",
+      handlers: ["console"],
+    },
+  },
+});
+
+const logger = log.getLogger("postcss_resource_route");
+
 export const handler = async (
   _req: Request,
   _ctx: HandlerContext,
 ): Promise<Response> => {
   const webservPath = _ctx.params.path;
   const filesystemPath = `static/${webservPath}`;
-  console.log(filesystemPath);
+  logger.debug(`PostCSS Transformed: ${filesystemPath}`);
   const fileContent = await Deno.readTextFile(filesystemPath);
   const result = await postCssInstance.process(fileContent, {
     from: filesystemPath,
