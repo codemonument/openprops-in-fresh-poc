e3b927aba60578b476b3f1acedd352f793d4e153
Benjamin Jesuiter <bjesuiter@gmail.com>
2022-12-03 01:10:33 +0100

    add new route especially for postcss

--- fresh.gen.ts
+++ fresh.gen.ts
@@ -5,12 +5,14 @@
 import config from "./deno.json" assert { type: "json" };
 import * as $0 from "./routes/index.tsx";
 import * as $1 from "./routes/postcss/[...path].ts";
+import * as $2 from "./routes/with-postcss.tsx";
 import * as $$0 from "./islands/Counter.tsx";
 
 const manifest = {
   routes: {
     "./routes/index.tsx": $0,
     "./routes/postcss/[...path].ts": $1,
+    "./routes/with-postcss.tsx": $2,
   },
   islands: {
     "./islands/Counter.tsx": $$0,
---
+++ routes/with-postcss.tsx
@@ -0,0 +1,27 @@
+import { Head } from "$fresh/runtime.ts";
+import Counter from "../islands/Counter.tsx";
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Fresh App With Postcss</title>
+        <link rel="stylesheet" href="/postcss/global.css"></link>
+      </Head>
+      <div>
+        <h1>With PostCSS</h1>
+        <img
+          src="/logo.svg"
+          width="128"
+          height="128"
+          alt="the fresh logo: a sliced lemon dripping with juice"
+        />
+        <p>
+          Welcome to `fresh`. Try updating this message in the ./routes/index.tsx
+          file, and refresh.
+        </p>
+        <Counter start={3} />
+      </div>
+    </>
+  );
+}
