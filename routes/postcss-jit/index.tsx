f85786a9751d5cbce8d65c70b28e84dd00643b59
Benjamin Jesuiter <bjesuiter@gmail.com>
2022-12-03 02:46:07 +0100

    add comment to postcss-jit

--- routes/postcss-jit/index.tsx
+++ routes/postcss-jit/index.tsx
@@ -1,6 +1,13 @@
 import { Head } from "$fresh/runtime.ts";
 import Counter from "../../islands/Counter.tsx";
 
+/**
+ * Good: Postcss-Jit Mode works as expected! Only the OpenProps get added, that are used in /static/postcss-jit.css. 
+ * 
+ * Bad: Normalize CSS not available, bc. it is not part of the main OpenProps object and it would not be used anyways, 
+ * so the additional css rules would not be loaded. 
+ * @returns 
+ */
 export default function Home() {
   return (
     <>
