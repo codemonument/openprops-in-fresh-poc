1b193744c84d1c40e853f87c88f7e3ddfb575e5b
Benjamin Jesuiter <bjesuiter@gmail.com>
2022-12-03 02:31:07 +0100

    add test animation in counter

--- islands/Counter.tsx
+++ islands/Counter.tsx
@@ -7,11 +7,24 @@ interface CounterProps {
 
 export default function Counter(props: CounterProps) {
   const [count, setCount] = useState(props.start);
+
+  const [style, setStyle] = useState({});
+
+  const increment = () => {
+    setCount(count + 1); 
+    setStyle({animation: 'var(--animation-fade-in-bloom) forwards'});
+  }
+
+  const decrement = () => {
+    setCount(count - 1); 
+    setStyle({});
+  }
+
   return (
     <div>
-      <p class="count">{count}</p>
-      <Button onClick={() => setCount(count - 1)}>-1</Button>
-      <Button onClick={() => setCount(count + 1)}>+1</Button>
+      <p style={style} class="count">{count}</p>
+      <Button onClick={decrement}>-1</Button>
+      <Button onClick={increment}>+1</Button>
     </div>
   );
 }
