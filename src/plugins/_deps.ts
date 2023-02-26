63d0ba76a0e7eead7b300c86c581151a8fdf2860
Benjamin Jesuiter <bjesuiter@gmail.com>
2023-02-26 15:52:56 +0100

    update DOMParser import

--- src/plugins/_deps.ts
+++ src/plugins/_deps.ts
@@ -1,6 +1,10 @@
 export * from "https://deno.land/x/zod@v3.20.5/mod.ts"
 
-export {
-    DOMParser,
-    initParser,
-} from "https://deno.land/x/deno_dom@v0.1.36-alpha/deno-dom-wasm-noinit.ts";
+export { DOMParser } from "https://deno.land/x/deno_dom@v0.1.36-alpha/deno-dom-wasm.ts";
+
+// If you don't want to init the wasm directly with top-level await 
+// but instead want to do it later 
+// export {
+//     DOMParser,
+//     initParser,
+// } from "https://deno.land/x/deno_dom@v0.1.36-alpha/deno-dom-wasm-noinit.ts";
