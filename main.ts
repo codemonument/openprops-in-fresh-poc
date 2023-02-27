51fe36af637cc5abb6e86d74fa01fc6d14e894c1
Benjamin Jesuiter <bjesuiter@gmail.com>
2023-02-27 02:39:08 +0100

    at start: make sure that the open-props files are available in css_deps folder

--- main.ts
+++ main.ts
@@ -8,6 +8,9 @@ import { start } from "$fresh/server.ts";
 import manifest from "./fresh.gen.ts";
 import { preprocessCss } from "./src/preprocess-css.ts";
 
+// make sure that the open-props files are available in css_deps folder 
+await (import('./scripts/download-openprops.ts'))
+
 // generate css files in /static via postcss (Test 04)
 preprocessCss()
 
